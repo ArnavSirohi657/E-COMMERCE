5a26997c5789579de918fc19ec2e64449551711a
Arnav Sirohi <arnavsirohi007@gmail.com>
2025-06-13 12:35:14 +0530

    final commit

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,14 +1,13 @@
 // utils/axiosInstance.js
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_BASE_URL || "https://e-commerce-fpf4.onrender.com/";
+const baseURL = import.meta.env.VITE_API_BASE_URL || "https://e-commerce-fpf4.onrender.com/api/products";
 
 const axiosInstance = axios.create({
   baseURL,
   headers: {
     "Content-Type": "application/json",
   },
-  withCredentials: true, // Only if you're using cookies or sessions
 });
 
 export default axiosInstance;
